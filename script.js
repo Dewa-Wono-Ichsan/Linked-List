01728592349ed81cc51ddc120429fc76a4e32357
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2025-01-28 07:46:20 +0700

    Add function for insert node at end of linked list
    Place function in scope of object constructor
    LinkedList.
    
    Add condition in function append. FIrst condition
    when head of linked list equal to null. Second
    condition when head of linked list not equal
    to null.
    
    Add execution code to insert node at the end of
    linked list structure. First condition insert
    node for head key and replace null value. Second
    condition iterate next value until arrive at next
    key equal to null, then replace null value with
    new node.

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function Node(value = null, next = null){
     this.value = value
     this.next = next
 }
+
+LinkedList.prototype.append = function(value){
+    if(this.head === null){
+
+        this.head = new Node(value)
+        return
+    }
+
+    let lastNode = this.head
+
+    if(this.head !== null){
+
+        while (lastNode.next !== null) {
+            lastNode = lastNode.next
+        }
+    }
+
+    return lastNode.next = new Node(value)
+}
+
+const list = new LinkedList()
+
+list.append('hamburger')
+list.append('spinach')
+
+console.log(list)
